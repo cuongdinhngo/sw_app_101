4a86d318044e4df30e315182ab492ba87f241c85
Cuong Ngo <55419546+cuongdinhngo@users.noreply.github.com>
2025-01-07 13:47:51 +0700

    feat: administration module

--- src/custom/plugins/SwagShopFinder/src/Resources/app/administration/src/module/swag-example/index.js
+++ src/custom/plugins/SwagShopFinder/src/Resources/app/administration/src/module/swag-example/index.js
@@ -1,7 +1,7 @@
 // <plugin root>/src/Resources/app/administration/src/module/swag-example/index.js
-import './page/swag-example-list';
-import './page/swag-example-detail';
-import './page/swag-example-create';
+// import './page/swag-example-list';
+// import './page/swag-example-detail';
+// import './page/swag-example-create';
 import deDE from './snippet/de-DE';
 import enGB from './snippet/en-GB';
 
@@ -19,23 +19,26 @@ Shopware.Module.register('swag-example', {
     },
 
     routes: {
-        list: {
-            component: 'swag-example-list',
-            path: 'list'
-        },
-        detail: {
-            component: 'swag-example-detail',
-            path: 'detail/:id',
-            meta: {
-                parentPath: 'swag.example.list'
-            }
-        },
-        create: {
-            component: 'swag-example-create',
-            path: 'create',
-            meta: {
-                parentPath: 'swag.example.list'
-            }
+        // list: {
+        //     component: 'swag-example-list',
+        //     path: 'list'
+        // },
+        // detail: {
+        //     component: 'swag-example-detail',
+        //     path: 'detail/:id',
+        //     meta: {
+        //         parentPath: 'swag.example.list'
+        //     }
+        // },
+        // create: {
+        //     component: 'swag-example-create',
+        //     path: 'create',
+        //     meta: {
+        //         parentPath: 'swag.example.list'
+        //     }
+        // }
+        a: {
+            component: 'b'
         }
     },
 
